92ba74416ab8a66486943a585b40f611b3bc0be6
Shahrizan <shahrizan.code@gmail.com>
2024-05-16 20:31:49 +0700

    chore: Add Prettier and ESLint configurations and dependencies.
    edit migration and model file.
    
    edit auth route
    
    add validations and exceptions folder

--- route/authUser.js
+++ route/authUser.js
@@ -1,8 +1,11 @@
 const express = require("express");
+
 const router = express.Router();
 const { login, googleLogin } = require("../controller/auth/authUser");
-//const { authMiddleware } = require("../middleware/auth");
+const { validationHandler } = require("../middleware");
+const { userLoginSchema } = require("../validations");
+// const { authMiddleware } = require("../middleware/auth");
 
-router.post("/login", login);
+router.post("/login", validationHandler(userLoginSchema), login);
 router.post("/google-login", googleLogin);
 module.exports = router;
---
+++ validations/index.js
@@ -0,0 +1,6 @@
+const { userLoginSchema, userRegisterSchema } = require("./UserSchema");
+
+module.exports = {
+  userLoginSchema,
+  userRegisterSchema,
+};
